45ed1fe03a69fb93dd6d37ed66f4848ca1b40ed2
elias <jg057440@gmai.com>
2023-06-15 08:41:39 -0600

    se agregaron dos archivos

---
+++ exa.py
@@ -0,0 +1,12 @@
+n1 = float (input("introduce tu primer numero"))
+n2 = float (float("introduce tu segundo numero"))
+
+opcion = 0
+while True:
+    print(""
+    dime, ¿que quieres hacer?
+        
+        1. suma dos numeros
+        2. Restar dos numeros
+        3. multiplicacion de dos numeros
+        4. Division   )
